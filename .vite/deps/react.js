c6984d0dd20766c687c7ebfb3fc3f3814313bf4d
Danila Pyzhikov <danila.pyzhikov@gmail.com>
2025-04-25 15:54:13 +0300

    feat: base loyout and

--- .vite/deps/react.js
+++
@@ -1,4 +0,0 @@
-import {
-  require_react
-} from "./chunk-II5XWRQS.js";
-export default require_react();
--- src/app/constants/router/routes.tsx
+++ src/app/constants/router/routes.tsx
@@ -1,3 +1,14 @@
 import { RouteObject } from 'react-router-dom';
 
-export const routes: RouteObject[] = [{}];
+import { PATHS } from '@constants/path.config';
+
+import MainLayout from '@/components/layouts/MainLayout';
+import Home from '@components/pages/Home/Home';
+
+export const routes: RouteObject[] = [
+  {
+    path: PATHS.HOME,
+    element: <MainLayout />,
+    children: [{ index: true, element: <Home /> }],
+  },
+];
