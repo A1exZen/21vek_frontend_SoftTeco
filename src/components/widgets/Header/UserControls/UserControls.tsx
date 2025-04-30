dbacddf09f5db09d10eed8f560c48884d699c030
Darya Babich <darya.babich11@gmail.com>
2025-04-30 04:13:32 +0300

    feat: add UserConntrols section

--- src/components/widgets/Header/UserControls/UserControls.tsx
+++ src/components/widgets/Header/UserControls/UserControls.tsx
@@ -1,6 +1,49 @@
+import UserControlsButton from "@/components/dummies/UserControlsButton";
+import styles from "./styles.module.scss";
+import Logo from "/src/assets/icons/main-logo.png";
+import { Input } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+
 const UserControls = () => {
-  return <>
-  </>;
+  return (
+    <div className={styles['header-info-container']}>
+
+      <div className={styles['logo-container']}>
+        <img src={Logo} alt="Логотип" className={styles["main-logo"]} />
+      </div>
+
+      <UserControlsButton 
+        icon={"nm"} 
+        text="Каталог товаров" 
+        to="/favorites" 
+      />
+
+      <div className={styles['search-container']}>
+        <Input 
+          size="large" 
+          placeholder="Поиск товаров..." 
+          prefix={<SearchOutlined />}
+          className={styles.searchInput}
+        />
+      </div>
+
+      <UserControlsButton 
+        icon={"nm"} 
+        text="Избранное" 
+        to="/favorites" 
+      />
+      <UserControlsButton 
+        icon={"nm"} 
+        text="Аккаунт" 
+        to="/favorites" 
+      />
+      <UserControlsButton 
+        icon={"nm"} 
+        text="Корзина" 
+        to="/favorites" 
+      />
+    </div>
+  );
 };
 
 export default UserControls;
