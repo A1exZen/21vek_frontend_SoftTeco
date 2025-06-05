6e9606835090b9458fd5015d36bc17cd2225f285
v0vchic <vladimir.bogdanovi04@gmail.com>
2025-06-05 12:32:24 +0300

    refactor(basket): correct class nesting structure

--- src/components/pages/Basket/constants.ts
+++ src/components/pages/Basket/constants.ts
@@ -3,21 +3,21 @@ import iphone from '@images/iphone.jpg';
 
 export const basketItems: BasketItemType[] = [
   {
-    id: crypto.randomUUID(),
+    id: Math.random().toString(36).slice(2, 10),
     image: iphone,
     name: 'Смартфон Apple iPhone 15 Pro 256GB',
     quantity: 1,
     price: 4399.0,
   },
   {
-    id: crypto.randomUUID(),
+    id: Math.random().toString(36).slice(2, 10),
     image: iphone,
     name: 'Смартфон Apple iPhone 15 Pro 256GB',
     quantity: 1,
     price: 4399.0,
   },
   {
-    id: crypto.randomUUID(),
+    id: Math.random().toString(36).slice(2, 10),
     image: iphone,
     name: 'Смартфон Apple iPhone 15 Pro 256GB',
     quantity: 1,
