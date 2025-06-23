144c73b3cbaf49e27aa7edd4c462b805dfb25b13
v0vchic <vladimir.bogdanovi04@gmail.com>
2025-06-23 18:37:17 +0300

    feat(checkout): add backend integration

--- src/hooks/useCheckout.ts
+++ src/hooks/useCheckout.ts
@@ -1,15 +1,16 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { RequestPlaceOrder } from '@models/checkout/api.ts';
-import { placeOrder } from '@/api/checkout.ts';
+import { RequestPlaceOrder } from '@models/checkout/api';
+import { placeOrder } from '@/api/checkout';
 import { toast } from 'react-hot-toast';
 import { QueryKeys } from '@/constants';
 
-export const usePlaceOrder = () => {
+export const useCheckout = () => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (orderData: RequestPlaceOrder) => placeOrder(orderData),
     onSuccess: async () => {
       await queryClient.invalidateQueries({ queryKey: [QueryKeys.BASKET] });
+      toast.success('Заказ оформлен');
     },
     onError: () => toast.error('Не удалось обновить данные'),
   });
--- src/models/checkout/api.ts
+++ src/models/checkout/api.ts
@@ -1,7 +1,4 @@
 export interface RequestPlaceOrder {
-  basket_items: {
-    id_us_storage: number;
-  }[];
   comment: string;
   adress: string;
   shipping_cost: number;
@@ -9,11 +6,7 @@ export interface RequestPlaceOrder {
 }
 
 export interface ResponsePlaceOrder {
-  basket_items: {
-    id_us_storage: number;
-  }[];
-  comment: string;
-  adress: string;
-  shipping_cost: number;
-  organization: string;
+  status: string;
+  message: string;
+  order_processor_id: number;
 }
